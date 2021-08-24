74c25e025a102b02e53ce123a176b34a2af9416b
Jonas Souza <jonasmzsouza@gmail.com>
2021-08-24 19:19:28 -0300

    ASP.NET Core - Rotas e Controller

---
+++ Fiap.Aula02.Web/Fiap.Aula02.Web.Exemplo01/Controllers/ProdutoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap.Aula02.Web.Exemplo01.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        //produto/index
+        //produto
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
