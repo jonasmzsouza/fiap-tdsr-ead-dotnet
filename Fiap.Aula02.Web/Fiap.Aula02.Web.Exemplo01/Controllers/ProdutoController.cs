51fee480e268f4db6ed49fefbb7c668ffcc6f3cf
Jonas Souza <jonasmzsouza@gmail.com>
2021-08-24 22:21:48 -0300

    ASP.NET Core - Rotas e Controller (continuacao)

--- Fiap.Aula02.Web/Fiap.Aula02.Web.Exemplo01/Controllers/ProdutoController.cs
+++ Fiap.Aula02.Web/Fiap.Aula02.Web.Exemplo01/Controllers/ProdutoController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fiap.Aula02.Web.Exemplo01.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,5 +15,34 @@ namespace Fiap.Aula02.Web.Exemplo01.Controllers
         {
             return View();
         }
+
+        //produto/cadastrar
+        [HttpGet] //Abrir a página com o formulário HTML
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost] //Recuperar os dados enviados pelo usuário (acionando após o submit do formulário)
+        public IActionResult Cadastrar(Produto produto)
+        {
+            //Enviar informações para a view: (Perde as informações no redirect)
+            ViewData["nome"] = $"Nome do produto através da ViewData = { produto.Nome}";
+            ViewData["preco"] = produto.Preco;
+            ViewBag.categoria = $"Categoria através da ViewBag {produto.Categoria}";
+            ViewBag.produto = produto; //envia todo o objeto Produto para a view
+
+            //TempData: mantém as informações após um redirect
+            TempData["mensagem"] = "Produto cadastrado!";
+            TempData["nome"] = produto.Nome;
+
+            //return RedirectToAction("Cadastrar"); //Redirect para o método (Action) Cadastrar
+            //Retorna para a view com o objeto Produto
+            return View(produto); //Forward
+            //return Content($"Nome: {produto.Nome}, Preço: {produto.Preco}, Categoria: {produto.Categoria}"); //Retorna um texto para o browser
+        }
+
     }
 }
+
+//CTRL + M + G
---
+++ Fiap.Aula02.Web/Fiap.Aula02.Web.Exemplo01/Controllers/UsuarioController.cs
@@ -0,0 +1,33 @@
+﻿using Fiap.Aula02.Web.Exemplo01.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap.Aula02.Web.Exemplo01.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Usuario usuario)
+        {
+            ViewData["nome"] = usuario.Nome;
+            ViewBag.dataNascimento = usuario.DataNascimento;
+            TempData["mensagem"] = "Usuário Cadastrado!";
+            return View(usuario);
+        }
+
+    }
+}
