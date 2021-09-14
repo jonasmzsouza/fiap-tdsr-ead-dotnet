f06bbf45ac0a8255e6a517d4c173b890cd8d26be
Jonas Souza <jonasmzsouza@gmail.com>
2021-09-14 19:28:45 -0300

    ASP.NET Core - Razor e Tag Helpers

---
+++ Fiap.Aula02.Web/Fiap.Aula02.Web.Exemplo01/Controllers/VeiculoController.cs
@@ -0,0 +1,89 @@
+﻿using Fiap.Aula02.Web.Exemplo01.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap.Aula02.Web.Exemplo01.Controllers
+{
+    public class VeiculoController : Controller
+    {
+        // Armazenar os veículos cadastrados, simular um DB
+        private static List<Veiculo> _banco = new List<Veiculo>();
+
+        // Aributo que armazena o di do veiculo
+        private static int id;
+
+        [HttpGet]
+        public IActionResult Remover(int id)
+        {
+            //Remover o veículo da lista
+            _banco.RemoveAt(_banco.FindIndex(v => v.Id == id));
+            //Mensagem de sucesso
+            TempData["msg"] = "Veículo removido!";
+            //Redirect para a listagem
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost] //Recebe os dados do fomulário e atualiza o veículo na lista
+        public IActionResult Editar(Veiculo veiculo)
+        {
+            //Atualizar o veicullo na lista
+            _banco[_banco.FindIndex(v => v.Id == veiculo.Id)] = veiculo;
+            //Mensagem de sucesso
+            TempData["msg"] = "Veículo atualizado!";
+            //Redirecionar para a listagem
+            return RedirectToAction("Index");
+        }
+
+        //URL: /veiculo/editar/id
+        [HttpGet] //Abrir a página de edição com o fomulário HTML preenchido
+        public IActionResult Editar(int id)
+        {
+            //Carregar as opções de cores do select
+            CarregarCores();
+            //Pesquisar o veiculo pelo id
+            var veiculo = _banco.Find(v => v.Id == id);
+            //Enviar o veículo para a view
+            return View(veiculo);
+        }
+
+        private void CarregarCores()
+        {
+            //Carregar as opções de cores do select
+            var lista = new List<string>(new string[] { "Prata", "Preto", "Vermelho", "Azul Metálico", "Branco" });
+            ViewBag.cores = new SelectList(lista);
+        }
+
+        //URL: /veiculo/index ou /veciulo
+        public IActionResult Index()
+        {
+            // Enviar a lista para  a view
+            return View(_banco);
+        }
+
+        //URL: /veiculo/cadastrar
+        [HttpGet] //Abrir a página com o fomulário HTML
+        public IActionResult Cadastrar()
+        {
+            //Carregar as opções de cores do select
+            CarregarCores();
+            return View();
+        }
+
+        [HttpPost] // Recupera os dados do formulário e adicionar na lista
+        public IActionResult Cadastrar(Veiculo veiculo)
+        {
+            // Setar o Id do veiculo
+            veiculo.Id = ++id;
+            // Adicionar o veiculo na lista
+            _banco.Add(veiculo);
+            // Mensagem de sucesso
+            TempData["msg"] = "Veículo cadastrado";
+            // Redirect
+            return RedirectToAction("cadastrar");
+        }
+    }
+}
---
+++ Fiap.Aula02.Web/Fiap.Aula02.Web.Exemplo01/Models/Veiculo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap.Aula02.Web.Exemplo01.Models
+{
+    public class Veiculo
+    {
+        public int Id { get; set; }
+        public string Modelo { get; set; }
+        public decimal Valor { get; set; }
+        public int Ano { get; set; }
+
+        [Display(Name = "Automático")]
+        public bool Automatico { get; set; }
+           
+        // Modifica o type do input (datatype) e o texto do label (display)
+        [DataType(DataType.Date), Display(Name = "Data de Compra")]
+        public DateTime DataCompra { get; set; }
+
+        public Combustivel Combustivel { get; set; }
+
+        public String Cor { get; set; }
+    }
+
+    public enum Combustivel
+    {
+        Etanol, Galosina, Flex, Diesel, Eletrico, Hibrido
+    }
+
+}
+
+
